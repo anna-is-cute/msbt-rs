1a3ef8a5185a96be97b5d15340b3a34871995cb7
Kyle Clemens <git@kyleclemens.com>
2019-03-04 12:40:46 -0500

    feat: implement Fail and Error on the error enum

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,21 @@
+use failure_derive::Fail;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, Fail)]
 pub enum Error {
+  #[fail(display = "io error: {}", _0)]
   Io(std::io::Error),
+  #[fail(display = "invalid magic bytes")]
   InvalidMagic,
+  #[fail(display = "invalid BOM")]
   InvalidBom,
+  #[fail(display = "invalid encoding: {}", _0)]
   InvalidEncoding(u8),
+  #[fail(display = "invalid utf-8: {}", _0)]
   InvalidUtf8(std::string::FromUtf8Error),
+  #[fail(display = "invalid utf-16: {}", _0)]
   InvalidUtf16(std::string::FromUtf16Error),
+  #[fail(display = "invalid section header: {:?}", _0)]
   InvalidSection([u8; 4]),
 }
