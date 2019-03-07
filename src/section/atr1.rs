6d7c36ff87a9aa30696a565b93b2ce05fc5e18c3
Kyle Clemens <git@kyleclemens.com>
2019-03-07 11:43:24 -0500

    refactor: begin refactor to make the API safer

---
+++ src/section/atr1.rs
@@ -0,0 +1,25 @@
+use crate::Msbt;
+use super::Section;
+
+use std::ptr::NonNull;
+
+#[derive(Debug)]
+pub struct Atr1 {
+  pub(crate) msbt: NonNull<Msbt>,
+  pub(crate) section: Section,
+  pub(crate) _unknown: Vec<u8>, // tons of unknown data
+}
+
+impl Atr1 {
+  pub fn msbt(&self) -> &Msbt {
+    unsafe { self.msbt.as_ref() }
+  }
+
+  pub fn section(&self) -> &Section {
+    &self.section
+  }
+
+  pub fn unknown_bytes(&self) -> &[u8] {
+    &self._unknown
+  }
+}
